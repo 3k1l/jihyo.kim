86ff9b552cf297dd5c9387c97a68d5d3e04c19c8
jihyoKim3929 <jihyo.kim3929@gmail.com>
2021-03-09 00:02:41 +0900

    Achieve 100% accuracy and performance : https://www.acmicpc.net/problem/1713

--- algorithm/후보추천하기.cpp
+++ algorithm/후보추천하기.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <list>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
@@ -7,29 +7,51 @@ void solution(int pictureCnt, int studentCnt, int recommands[1000]) {
     int recommandedStudent;
     int recommandCnt[1000] = {0};
     int studentINOUT[1000] = {0};
-    int listUp[pictureCnt];
-    int minStudent = recommands[0];
-    int cnt = 0;
+    vector<pair<int,int> > listUp;
+    int minStudent, minIdx;
+    int minCnt, minTime;
 
     for(int i = 0 ; i < studentCnt; ++i) {
         recommandedStudent = recommands[i];
+        //cout<<"rec student : "<<recommandedStudent<<'\n';
+        recommandCnt[recommandedStudent]++;
         if(studentINOUT[recommandedStudent] == 0) {
-            if(cnt == pictureCnt) {
-                int* p = find(listUp, listUp+pictureCnt, minStudent);
-                p = recommandedStudent;
+            if(listUp.size() == pictureCnt) {
+                minCnt = 1000;
+                minIdx = 0;
+                minTime = 1000;
+                for(int j = 0 ; j < listUp.size() ; ++j) {
+                    int student = listUp[j].first;
+                    int time = listUp[j].second;
+                    if(recommandCnt[student] < minCnt) {
+                        minCnt = recommandCnt[student];
+                        minStudent = student;
+                        minIdx = j;
+                        minTime = time;
+                    }
+                    else if (recommandCnt[student] == minCnt) {
+                        if(time < minTime) {
+                            minCnt = recommandCnt[student];
+                            minStudent = student;
+                            minIdx = j;
+                            minTime = time;
+                        }
+                    }
+
+                }
+                listUp[minIdx] = make_pair(recommandedStudent,i);
+                recommandCnt[minStudent] = 0;
+                studentINOUT[minStudent] = 0;                
             }
             else {
-                listUp[cnt] = recommandedStudent;
+                listUp.push_back(make_pair(recommandedStudent, i));
             }
-        }
-        else {
-            ++cnt;  
-        }      
-        ++recommandCnt[recommandedStudent];
-        if(recommandCnt[minStudent] > recommandCnt[recommandedStudent]) {
-            minStudent = recommandedStudent;
-        }
+            studentINOUT[recommandedStudent] = 1;
+        }//end of studentINOUT
     }
+    sort(listUp.begin(), listUp.end());
+    for(int i = 0 ; i < listUp.size() ; ++i)
+        cout<<listUp[i].first<<' ';
 }
 
 int main() {
@@ -40,5 +62,6 @@ int main() {
     for(int i = 0 ; i < studentCnt ; ++i)
         scanf("%d", &recommands[i]);
     solution(pictureCnt, studentCnt, recommands);
+
     return 0;
 }
