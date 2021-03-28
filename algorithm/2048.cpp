595d33417ba15cf6469d544d6a14c67878892ab3
jihyoKim3929 <jihyo.kim3929@gmail.com>
2021-03-28 22:53:29 +0900

    Need to update codes

--- algorithm/2048.cpp
+++ algorithm/2048.cpp
@@ -1,27 +1,117 @@
 #include <iostream>
 #include <vector>
 
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGTH 3
+
 using namespace std;
+int answer = 0;
 
-int dx[4] = {0,0,-1,1}; //UP, DOWN, LEFT, RIGTH
-int dy[4] = {-1,1,0,0}; //UP, DOWN, LEFT, RIGTH
+void findMax(int board[20][20], int boardRow) {
+    for(int i = 0 ; i < boardRow ; ++i) {
+        for(int j = 0 ; j < boardRow ; ++j) {
+            answer= max(answer, board[i][j]);
+        }
+    }
+    cout<<"findMax : "<< answer<<'\n';
+}
 
-int solution(int board[20][20]) {
-    int answer = 0;
+void moveBlock(int board[20][20], int boardRow, int cnt, int way) {
+    int boardTemp[20][20];
+    int row,col;
+    row = col = 0;
+    //end point
+    if(cnt == 0) {
+        findMax(board, boardRow);
+        return;
+    }
 
+    if(way == UP) {
+        //initialize
+        row = 0; 
+        for(int i = 0 ; i < boardRow ; ++i) {
+            for(int j = 0 ; j < boardRow; ++j) {
+                boardTemp[i][j] = board[i][j];
+            }
+        }
+        //move point
+        for(int i = 0 ; i < boardRow ; ++i) {
+            for(int j = 0 ; j < boardRow ; ++j) {
+                //check below board is same
+                if(board[i][j] == 0) 
+                    continue;
+                
+                if(i < boardRow-1) {
+                    if(board[i+1][j] == board[i][j]) {
+                        board[i][j] *= 2;
+                        board[i+1][j] = 0;
+                        continue;
+                    }
+                }
+                if(board[row][j] == 0) {
+                    board[row][j] = board[i][j];
+                    board[i][j] = 0;
+                }
+            }
+        }
+        //call to next step
+        for(int i = 0 ; i < 4 ; ++i) {
+            moveBlock(boardTemp, boardRow, cnt-1, i);
+        }
+    }
+    else if(way == DOWN) {
+        for(int i = 0 ; i < boardRow ; ++i) {
+            for(int j = 0 ; j < boardRow; ++j) {
+                boardTemp[i][j] = board[i][j];
+            }
+        }
+        for(int i = 0 ; i < 4 ; ++i) {
+            moveBlock(boardTemp, boardRow, cnt-1, i);
+        }
+    }
+    else if(way == LEFT) {
+        for(int i = 0 ; i < boardRow ; ++i) {
+            for(int j = 0 ; j < boardRow; ++j) {
+                boardTemp[i][j] = board[i][j];
+            }
+        }
+        for(int i = 0 ; i < 4 ; ++i) {
+            moveBlock(boardTemp, boardRow, cnt-1, i);
+        }
+    }
+    else if(way == RIGTH) {
+        for(int i = 0 ; i < boardRow ; ++i) {
+            for(int j = 0 ; j < boardRow; ++j) {
+                boardTemp[i][j] = board[i][j];
+            }
+        }
+        for(int i = 0 ; i < 4 ; ++i) {
+            moveBlock(boardTemp, boardRow, cnt-1, i);
+        }
+    }
+    
+}
 
-    return answer;
+void solution(int board[20][20], int boardRow) {
+    int cnt = 5;
+    int num;
+
+    for(int i = 0 ; i < 4 ; ++i) {
+        moveBlock(board, boardRow, cnt, i);//way == i 
+    }
 }
 
 int main() {
     int boardRow;
-    int board[20][20];
+    int board[20][20] = {0};
     scanf("%d", &boardRow );
     for(int i = 0 ; i < boardRow ; ++i) {
         for(int j = 0 ; j < boardRow; ++j) {
             scanf("%d", &board[i][j]);
         }
     }
-    cout<<solution(board);
+    cout<<answer;
     return 1;
 }
