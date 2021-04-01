80f01cbb275d339c66a4fb499c8b6cf5041c1d03
jihyoKim3929 <jihyo.kim3929@gmail.com>
2021-04-01 22:03:26 +0900

    Achieve 100% performance and accuracy : https://www.acmicpc.net/problem/3190

--- algorithm/뱀.cpp
+++ algorithm/뱀.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-
+//apples
 int applesRow[100] = {-1};
 int applesCol[100] = {-1};
 int appleNum = 0;
-
+//boards
+int boards[101][101]= {0};
+int boardSize;
+//snake
+queue<pair<int, int> > snake;
+//commands
+int commandNum;
+int commandSeconds[100];
+char commandDirections[100];
+//direction
 enum Direction
 {
     RIGHT, 
@@ -14,8 +23,9 @@ enum Direction
     DOWN
 };
 
-bool checkApple(int x, int y ) {
+void checkApple(int x, int y ) {
     bool appleEat = false;
+    int tailX, tailY;
     for(int i  = 0 ; i < appleNum ; ++i) {
         if(applesRow[i] == x && applesCol[i] == y) {
             appleEat = true;
@@ -24,63 +34,69 @@ bool checkApple(int x, int y ) {
             break;
         }
     }
-    return appleEat;
+    if(!appleEat) {
+        tailX = snake.front().first;
+        tailY = snake.front().second;
+        boards[tailX][tailY] = 0;
+        snake.pop();
+    }
+}
+
+bool checkDie(int headX, int headY) {
+    if(headX > boardSize || headX <=0 || headY > boardSize || headY <=0) {
+        return true;
+    }
+    if(boards[headX][headY] == 1) {
+        return true;
+    }
+    return false;
+}
+
+bool checkCommand(int time, int commandIndex) {
+    if(commandNum <= commandIndex)
+        return false;
+    if(commandSeconds[commandIndex] == time) 
+        return true;
+    return false;
 }
 
 int main() {
-    int boardSize, directionNum;
-    int commandSeconds[100];
-    char commandDirections[100];
-    int boards[101][101]= {0};
     int direction = RIGHT;
     int commandIndex;
     int time;
-    int tailX, tailY, headX, headY;
-    queue<pair<int, int> > snake;
+    int headX, headY;
+    
     commandIndex = time = 0;
     headX = headY = 1;
-    boards[tailX][tailY] = 1;
-    snake.push(make_pair(tailX, tailY));
+    boards[headX][headX] = 1;
+    snake.push(make_pair(headX, headX));
     //intialize
     cin >> boardSize >> appleNum;
     for(int i = 0 ; i < appleNum ; ++ i) {
         cin >> applesRow[i];
         cin >> applesCol[i];
     }
-    cin >> directionNum;
-    for(int i = 0 ; i < directionNum ; ++ i) {
+    cin >> commandNum;
+    for(int i = 0 ; i < commandNum ; ++ i) {
         cin >> commandSeconds[i];
         cin >> commandDirections[i];
     }
     //solution
     while(true) {
         time++;
-        //Right D -> Down L ->Up
-        //Left D -> Up L -> Down
-        //Up D -> Right L -> LEFT
-        //Down D -> Left L -> right
         if(direction == RIGHT) {
             //move to right
             headY++;
-            if((headX > boardSize || headX <=0) || (headY > boardSize || headY <=0)) {
-                break;
-            }
-            if(boards[headX][headY] == 1) {
+            if(checkDie(headX, headY)) {
                 break;
             }
             boards[headX][headY] = 1;
             snake.push(make_pair(headX,headY));
-            //check apple is there
             //if there is no apple then remove before location's tail
             //and update tail location X and Y to 
-            if(!checkApple(headX, headY)) {
-                tailX = snake.back().first;
-                tailY = snake.back().second;
-                boards[tailX][tailY] = 0;
-                snake.pop();
-            }
+            checkApple(headX, headY);
             //check command is equal time. if time is equal commandSecond. update direction
-            if(commandSeconds[commandIndex] == time) {
+            if(checkCommand(time, commandIndex)) {
                 if(commandDirections[commandIndex] == 'D') {
                     direction = DOWN;
                 }
@@ -90,28 +106,19 @@ int main() {
                 ++commandIndex;
             }
         } // end of right   
-        if(direction == LEFT) {
+        else if(direction == LEFT) {
             //move to right
             headY--;
-            if((headX > boardSize || headX <=0) || (headY > boardSize || headY <=0)) {
-                break;
-            }
-            if(boards[headX][headY] == 1) {
+            if(checkDie(headX, headY)) {
                 break;
             }
             boards[headX][headY] = 1;
             snake.push(make_pair(headX,headY));
-            //check apple is there
             //if there is no apple then remove before location's tail
             //and update tail location X and Y to 
-            if(!checkApple(headX, headY)) {
-                tailX = snake.back().first;
-                tailY = snake.back().second;
-                boards[tailX][tailY] = 0;
-                snake.pop();
-            }
+            checkApple(headX, headY);
             //check command is equal time. if time is equal commandSecond. update direction
-            if(commandSeconds[commandIndex] == time) {
+            if(checkCommand(time, commandIndex)) {
                 if(commandDirections[commandIndex] == 'D') {
                     direction = UP;
                 }
@@ -121,28 +128,19 @@ int main() {
                 ++commandIndex;
             }
         } // end of LEFT
-       if(direction == UP) {
+       else if(direction == UP) {
             //move to right
             headX--;
-            if((headX > boardSize || headX <=0) || (headY > boardSize || headY <=0)) {
-                break;
-            }
-            if(boards[headX][headY] == 1) {
+            if(checkDie(headX, headY)) {
                 break;
             }
             boards[headX][headY] = 1;
             snake.push(make_pair(headX,headY));
-            //check apple is there
             //if there is no apple then remove before location's tail
             //and update tail location X and Y to 
-            if(!checkApple(headX, headY)) {
-                tailX = snake.back().first;
-                tailY = snake.back().second;
-                boards[tailX][tailY] = 0;
-                snake.pop();
-            }
+            checkApple(headX, headY);
             //check command is equal time. if time is equal commandSecond. update direction
-            if(commandSeconds[commandIndex] == time) {
+            if(checkCommand(time, commandIndex)) {
                 if(commandDirections[commandIndex] == 'D') {
                     direction = RIGHT;
                 }
@@ -152,28 +150,18 @@ int main() {
                 ++commandIndex;
             }
         } // end of UP
-       if(direction == DOWN) {
+       else if(direction == DOWN) {
             //move to right
             headX++;
-            if((headX > boardSize || headX <=0) || (headY > boardSize || headY <=0)) {
-                break;
-            }
-            if(boards[headX][headY] == 1) {
+            if(checkDie(headX, headY)) {
                 break;
             }
             boards[headX][headY] = 1;
             snake.push(make_pair(headX,headY));
-            //check apple is there
             //if there is no apple then remove before location's tail
-            //and update tail location X and Y to 
-            if(!checkApple(headX, headY)) {
-                tailX = snake.back().first;
-                tailY = snake.back().second;
-                boards[tailX][tailY] = 0;
-                snake.pop();
-            }
+            checkApple(headX, headY);
             //check command is equal time. if time is equal commandSecond. update direction
-            if(commandSeconds[commandIndex] == time) {
+            if(checkCommand(time, commandIndex)) {
                 if(commandDirections[commandIndex] == 'D') {
                     direction = LEFT;
                 }
@@ -185,5 +173,5 @@ int main() {
         } // end of LEFT
     }
     cout<< time <<endl;
-    return 1;
+    return 0;
 }
